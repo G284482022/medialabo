bf935e69390914d5458a6abf8b1ad19eaf54e666
G284482022 <228448@st.takushoku-u.ac.jp>
2023-05-24 16:34:59 +0900

    課題第4回

--- Kazuate.js
+++ Kazuate.js
@@ -2,34 +2,35 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 let kaisu = 0;
 hantei();
-hantei();
-hantei();
-hantei();
 function hantei() {
-        let yoso = 4;
-        kaisu = kaisu+1;
-        console.log(kaisu);
-        console.log(kaisu+"回目の予想:"+yoso);
-        if(kaisu<3){
-            if(yoso<kotae){
-                console.log("まちがい.答えはもっと大きいですよ");
-            }else if(yoso>kotae){
-                console.log("まちがい.答えはもっと小さいですよ");
-            }if((kaisu-1)&&yoso==kotae){
-                console.log("答えは "+kotae+" でした. すでにゲームは終わっています");
-            }else if(yoso===kotae){
-                console.log("正解です.おめでとう!");
-            }
-        }if(kaisu===3){
-            if((kaisu-1)&&yoso===kotae){
-                console.log("答えは "+kotae+" でした. すでにゲームは終わっています");
-            }else if(yoso===kotae){
-                console.log("正解です.おめでとう!");
-            }else{
-            console.log("まちがい.残念でした答えは "+kotae+" です.");
-            }
+    
+    kaisu = kaisu+1;
+    let e1 = document.querySelector('span#kaisu');
+    e1.textContent = kaisu;
+    let yoso = 4; 
+    let e2 = document.querySelector('span#answer');
+    e2.textContent = yoso;
+
+    let e3 = document.querySelector('p#result');
+    if(kaisu<3){
+        if(yoso<kotae){
+            e3.textContent = 'まちがい.答えはもっと大きいですよ';
+        }else if(yoso>kotae){
+            e3.textContent = 'まちがい.答えはもっと小さいですよ';
+        }if((kaisu-1)&&yoso==kotae){
+            e3.textContent = '答えは '+kotae+' でした. すでにゲームは終わっています';
+        }else if(yoso===kotae){
+            e3.textContent = '正解です.おめでとう!';
         }
-        if(kaisu>=4){
-            console.log("答えは "+kotae+" でした. すでにゲームは終わっています");
+    }if(kaisu===3){
+        if((kaisu-1)&&yoso===kotae){
+            e3.textContent = '答えは '+kotae+' でした. すでにゲームは終わっています';
+        }else if(yoso===kotae){
+            e3.textContent = '正解です.おめでとう!';
+        }else{
+            e3.textContent = 'まちがい.残念でした答えは '+kotae+' です.';
         }
+    }if(kaisu>=4){
+        e3.textContent = '答えは '+kotae+' でした. すでにゲームは終わっています';
+    } 
 }
